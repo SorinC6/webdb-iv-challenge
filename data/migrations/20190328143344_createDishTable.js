a0c28a381ab071497aa93d82ba9ec653c07c9adf
Sorin Chis <kish.sorin@yahoo.com>
2019-03-28 14:46:29 +0200

    create knex schema for dishes table

--- data/migrations/20190328143344_createDishTable.js
+++ data/migrations/20190328143344_createDishTable.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+	return knex.schema.createTable('dishes', (table) => {
+		table.increments();
+
+		table.string('dish_name').notNullable().unique();
+
+		table.timestamps(true, true);
+	});
 };
 
 exports.down = function(knex, Promise) {
-  
+	return knex.schema.dropTableIfExists('dish');
 };
