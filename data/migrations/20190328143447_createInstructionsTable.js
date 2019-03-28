3708370a29ae78cc3e80e9262392294015e47eab
Sorin Chis <kish.sorin@yahoo.com>
2019-03-28 15:15:17 +0200

    create knex schema for instruction table

--- data/migrations/20190328143447_createInstructionsTable.js
+++ data/migrations/20190328143447_createInstructionsTable.js
@@ -1,9 +1,14 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable
-  
+	return knex.schema.createTable('instructions', (table) => {
+		table.increments();
+
+		table.string('instructions').notNullable();
+		table.integer('order').unsigned();
+		table.integer('recipe_id').unsigned();
+		table.foreign('recipe_id').references('id').inTable('recipes').onDelete('CASCADE').onUpdate('CASCADE');
+	});
 };
 
 exports.down = function(knex, Promise) {
-  
+	return knex.schema.dropTableIfExists('dish');
 };
