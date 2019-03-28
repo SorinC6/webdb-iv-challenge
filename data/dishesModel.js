82cb904a5306a0856cc22dd0361df7e8ceda97bd
Sorin Chis <kish.sorin@yahoo.com>
2019-03-28 20:41:39 +0200

    rewriting function for dishModel

--- data/dishesModel.js
+++ data/dishesModel.js
@@ -2,33 +2,16 @@ const knex = require('knex');
 const dbConfig = require('../knexfile');
 const db = knex(dbConfig.development);
 
-function get(id) {
-	if (id) {
-		return db('dishes').where('id', id).then((dish) => {
-			return dish[0];
-		});
-	}
-
-	return db('dishes').then((dishes) => {
-		return dishes;
-	});
+function getDishes() {
+	return db('dishes');
 }
 
-function getDish(id) {
-	return db
-		.select('id', 'dishes.dish_name', 'recipes.recipes_name')
-		.from('dishes')
-		.join('recipes', 'dishes.id', '=', 'recipes.id');
+function getDishById(id) {
+	return db('dishes').where({ id });
 }
 
-function add(dish) {
-	return db('dishes').insert(dish).then(([ id ]) => this.get(id));
+function addDish(dish) {
+	return db('dishes').insert(dish);
 }
 
-function getAllDishes() {}
-
-module.exports = {
-	get,
-	getDish,
-	add
-};
+module.exports = { getDishes, getDishById, addDish };
