d0459087685d21d79e21d4b51fbcb5845fb89ef0
Sorin Chis <kish.sorin@yahoo.com>
2019-03-28 16:18:42 +0200

    added endpoint for posting a dish to database

--- api/routes/dish-router.js
+++ api/routes/dish-router.js
@@ -19,11 +19,26 @@ router.get('/:id', async (req, res) => {
 		const dish = await db('dishes').where({ id: req.params.id }).first();
 		if (dish) {
 			res.status(200).json(dish);
-		}else{
-            res.status(404).json({message: ""})
-        }
+		} else {
+			res.status(404).json({ message: '' });
+		}
 	} catch (error) {
 		res.status({ error: 'error trying to get the data from database' });
+	}
+});
+
+router.post('/', async (req, res) => {
+	const body = req.body;
+	if (body.dish_name) {
+		try {
+			const [ result ] = await db('dishes').insert(req.body);
+			const dish = await db('dishes').where({ id: result }).first();
+			res.status(201).json(dish);
+		} catch (error) {
+			res.status(500).json({ error: 'error trying to save the dish in database' });
+		}
+	} else {
+		res.status(400).json({ error: 'please provide a name' });
 	}
 });
 
