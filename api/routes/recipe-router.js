273bd01413115f7aa256e521298e8589950e9e79
Sorin Chis <kish.sorin@yahoo.com>
2019-03-28 20:59:47 +0200

    function for getAll, get by Id and post working for recipes table

--- api/routes/recipe-router.js
+++ api/routes/recipe-router.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-const knex = require('knex');
-const dbConfig = require('../../knexfile');
-const db = knex(dbConfig.development);
+const recipeHelper = require('../../data/recipeModel');
 
 router.get('/', async (req, res) => {
 	try {
-		const recipes = await db('recipes');
+		const recipes = await recipeHelper.getRecipes();
 		res.status(200).json(recipes);
 	} catch (error) {
 		res.status(500).json({ error: 'thare was a error ' });
+	}
+});
+
+router.get('/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const recipe = await recipeHelper.getRecipeById(id);
+		res.status(200).json(recipe);
+	} catch (error) {
+		res.status(500).json({ error: 'error trying to get the data' });
 	}
 });
 
@@ -18,7 +27,7 @@ router.post('/', async (req, res) => {
 	const body = req.body;
 	if (body.recipe_name) {
 		try {
-			const [ result ] = await db('recipes').insert(req.body);
+			const result = await recipeHelper.addRecipe(body);
 			res.status(200).json(result);
 		} catch (error) {
 			res.status(500).json({ error: 'error trying to save in database' });
--- data/recipeModel.js
+++ data/recipeModel.js
@@ -2,5 +2,16 @@ const knex = require('knex');
 const dbConfig = require('../knexfile');
 const db = knex(dbConfig.development);
 
+function getRecipes() {
+	return db('recipes');
+}
 
-module.exports = {};
+function getRecipeById(id) {
+	return db('recipes').where({ id }).first();
+}
+
+function addRecipe(recipe) {
+	return db('recipes').insert(recipe);
+}
+
+module.exports = { getRecipes, getRecipeById, addRecipe };
